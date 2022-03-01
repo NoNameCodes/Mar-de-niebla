495722f9b6f524cf46a268175c2ecbe362bae6eb
SashaBidardel <sashabidardel@gamil.com>
2022-03-01 19:06:57 +0100

    adding header and footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="text-center p-12 mt-8 text-white " style="background-color: black">
+  <p  class="h3"> Fundación por la Acción Social Mar de niebla - 2022</p>
+    
+ </footer>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,11 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+  <header class="p-12 mb-4  text-warning" style="background-color: black">
+    
+  <div class="d-flex flex-row justify-content-between  align-items-center" >
+    <img src="{{ asset('storage/logo.jpg') }}" alt="logo">
+   
+     hola
+  </div>
+  
+  </header>
