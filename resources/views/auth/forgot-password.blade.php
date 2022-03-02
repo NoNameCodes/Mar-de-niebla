36f01914146bd37d23c2a122aa58eee7c786b539
Cortina17 <diego.cortinagarcia@gmail.com>
2022-03-02 11:54:21 +0100

    Style
    Style modified in login and register pages

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -5,7 +5,7 @@
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+            {{ __('¿Olvidaste tu contreseña? No hay problema. Introduce tu cuenta de email y te enviaremos un enlace de recuperación para que puedas elegir otra.') }}
         </div>
 
         @if (session('status'))
@@ -20,13 +20,12 @@
             @csrf
 
             <div class="block">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus placeholder="Introduce tu email" />
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 <x-jet-button>
-                    {{ __('Email Password Reset Link') }}
+                    {{ __('Enlace de recuperación') }}
                 </x-jet-button>
             </div>
         </form>
