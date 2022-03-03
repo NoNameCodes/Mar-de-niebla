98c787db15c28f15f7b5248daf60adfc19901568
Caio Da Silva <caiodasilvatx@gmail.com>
2022-03-03 16:56:05 +0100

    new view for the admin to control the users list

--- resources/views/admin-dashboard.blade.php
+++ resources/views/admin-dashboard.blade.php
@@ -7,48 +7,69 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-md-8">
-                            <div class="card">
-                                <div class="card-header">Users List to Approve</div>
-
-                                <div class="card-body">
-
-                                    @if (session('message'))
-                                        <div class="alert alert-success" role="alert">
-                                            {{ session('message') }}
-                                        </div>
-                                    @endif
-
-                                    <table class="table">
+            @if (session('message'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('message') }}
+                </div>
+            @endif
+            <div class="flex flex-col">
+                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div class="shadow overflow-scroll border-b border-gray-200 rounded-lg">
+                            <table class="min-w-full divide-y divide-gray-200">
+                                <thead class="bg-gray-50">
+                                    <tr>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Nombre</th>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Email</th>
+                                        <th scope="col"
+                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Fecha de Creación</th>
+                                        <th scope="col" class="relative px-6 py-3">
+                                            <span class="sr-only"></span>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                @forelse ($users as $user)
+                                    <tbody class="bg-white divide-y divide-gray-200">
                                         <tr>
-                                            <th>User name</th>
-                                            <th>Email</th>
-                                            <th>Registered at</th>
-                                            <th></th>
+                                            <td class="px-6 py-4 whitespace-nowrap">
+                                                <div class="flex items-center">
+                                                    <div class="flex-shrink-0 h-10 w-10">
+                                                        <img class="h-10 w-10 rounded-full" src="" alt="">
+                                                    </div>
+                                                    <div class="ml-4">
+                                                        <div class="text-sm font-medium text-gray-900">
+                                                            {{ $user->name }}</div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap">
+                                                <div class="text-sm text-gray-900">
+                                                    {{ $user->email }}</div>
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap">
+                                                <span
+                                                    class="px-2 inline-flex text-xs leading-5 font-semibold">{{ $user->created_at }}</span>
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                <a href="{{ route('admin.users.delete', $user->id) }}"
+                                                    class="text-indigo-600 hover:text-indigo-900">Eliminar</a>
                                         </tr>
-                                        @forelse ($users as $user)
-                                            <tr>
-                                                <td>{{ $user->name }}</td>
-                                                <td>{{ $user->email }}</td>
-                                                <td>{{ $user->created_at }}</td>
-                                                <td><a href="{{ route('admin.users.approve', $user->id) }}"
-                                                        class="btn btn-primary btn-sm">Approve</a></td>
-                                            </tr>
-                                        @empty
-                                            <tr>
-                                                <td colspan="4">No users found.</td>
-                                            </tr>
-                                        @endforelse
-                                    </table>
-                                </div>
-                            </div>
+                                    @empty
+                                        <tr>
+                                            <td>No users found.</td>
+                                        </tr>
+                                    </tbody>
+                                @endforelse
+                            </table>
                         </div>
                     </div>
                 </div>
             </div>
+            </table>
         </div>
-    </div>
 </x-app-layout>
