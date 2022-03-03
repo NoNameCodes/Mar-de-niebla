9e521937906087e850dd9018b9476d687b55dd4e
irina-p-d <irinapetrovap@gmail.com>
2022-03-03 12:41:33 +0100

    card ready for merge

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -13,11 +13,11 @@
 <div class="flex flex-col justify-center h-screen">
 	<div class="relative flex flex-row md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white">
 		<div class="w-full md:w-1/3 bg-white grid place-items-center">
-			<img src="https://images.pexels.com/photos/4381392/pexels-photo-4381392.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=300" alt="tailwind logo" class="rounded-xl" />
+			<img src="https://images.pexels.com/photos/4381392/pexels-photo-4381392.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=300" alt="tailwind logo" class="rounded-xl w-3/4" />
         </div>
         <div class="w-full md:w-2/3 bg-white flex flex-row space-y-2 p-3">
             
-            <p class="font-black text-gray-800 md:text-3xl text-xl align-middle">Nombre de recurso</p>
+            <p class="font-black text-gray-800 md:text-3xl text-xl">Nombre de recurso</p>
             <div class="user-option mx-auto sm:ml-auto sm:mr-0">
                 <button class="btn inline-block select-none no-underline align-middle cursor-pointer whitespace-nowrap px-4 py-1.5 rounded text-base font-medium leading-6 tracking-tight text-white text-center border-0 bg-[#92C954] hover:bg-[#85f307] duration-300" type="button">Reservar</button>
             </div>
