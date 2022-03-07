cebf2571da74b3a821e3c842ed801fc473822d0f
irina-p-d <irinapetrovap@gmail.com>
2022-03-07 12:01:39 +0100

    little changes of style

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -26,7 +26,7 @@
 
 
 <!-- component -->
-{{-- <div class="flex flex-col justify-center h-screen">
+ <div class="flex flex-col justify-center h-screen">
 	<div class="relative flex flex-row md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white">
 		<div class="w-full md:w-1/3 bg-white grid place-items-center">
 			<img src="https://images.pexels.com/photos/4381392/pexels-photo-4381392.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=300" alt="tailwind logo" class="rounded-xl w-3/4" />
@@ -39,7 +39,7 @@
             </div>
         </div>
 	</div>
-</div> --}}
+</div> 
 
 
 
