df6040ca8d302d20cd02dfeb3b51466939825048
SashaBidardel <sashabidardel@gamil.com>
2022-03-07 15:32:02 +0100

    adding footer down in reserve page

--- resources/views/reserve.blade.php
+++ resources/views/reserve.blade.php
@@ -11,22 +11,33 @@
 
       <x-header >
        <div clase="flex">
-        <form method="POST" action="{{ route('logout') }}">
-          @csrf
-          
-          <button type="submit"><i class="fa-solid fa-user fa-2x" style="color: #F8981D" ></i></button>
-          
-         
-        </form>
-        
-      </div>
-      </x-header > 
+            <button  type="button" data-dropdown-toggle="dropdown"><i class="fa-solid fa-user fa-2x" style="color: #F8981D" ></i> </button>
 
-      </div>
+            <!-- Dropdown menu -->
+            <div class="hidden bg-[#F8981D] text-base z-50  " id="dropdown">
+              
+                <ul class="py-1" aria-labelledby="dropdown">
+                <li>
+                    <a href="{{ route('dashboard') }}" class="text-sm hover:bg-gray-300 text-gray-700 block px-4 py-2">Dashboard</a>
+                </li>
+                <li>
+                  <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit" class="text-sm hover:bg-gray-300 text-gray-700 block px-4 py-2">Logout</button>
+                    </form>
+                </li>
+              
+                </ul>
+        </div>
+         <script src="https://unpkg.com/@themesberg/flowbite@latest/dist/flowbite.bundle.js"></script>
+
+    
+      </x-header >
       
-       <form action="">
+      
+       <form action=""class="footerdown ">
         
-        <div class="flex items-center justify-center mt-8 mb-4 ">
+        <div class="flex items-center justify-center mt-8 mb-4 " >
               <div class="flex flex-col ">
               <img src="{{ asset('storage/furgoneta.PNG') }}" alt="recurso" class="mb-5">
               
@@ -35,14 +46,14 @@
                 <label for="date" class="text-gray-700 italic"><p  class="flex justify-items-center" >Seleccione el día</p></label>
               
                 <input  type="date"
-                  class="form-control  text-gray-700 border-orange-500 rounded-xl py-2 px-4 bg-[#FAF3F3]"
+                  class="form-control  text-gray-700   border-orange-500 rounded-xl py-2 px-4 bg-[#FAF3F3]"
                     id="date"/> 
               
               </div>
         </div>
         </div>
         <div class="flex flex-col items-center justify-center mt-5 ">
-          <input type="text" placeholder="Introduce tu nombre" class="text-gray-700 italic border-orange-500 rounded-xl py-2 px-24 bg-[#FAF3F3] mb-5 	"  >
+          <input type="text" placeholder="Introduce tu nombre" class="text-gray-700 italic border-orange-500 rounded-xl py-2 px-24  bg-[#FAF3F3] mb-5 	"  >
           <input type="number"   placeholder="Introduce tu teléfono" class="text-gray-700 italic border-orange-500 rounded-xl py-2 px-24 bg-[#FAF3F3] mb-5 	"  >
           <textarea class="text-gray-700 italic border-orange-500 rounded-xl py-14 px-24 bg-[#FAF3F3] mb-5" placeholder="Comentarios" ></textarea>
           <button class="bg-[#F8981D] hover:bg-[#ecc188] text-white font-bold py-2 px-4 rounded-xl mb-5 shadow-xl" type="submit">
@@ -50,7 +61,8 @@
           </button>
         </div>
      
-      </form>     
+      </form> 
+          
         <x-footer />
 
     </body>
