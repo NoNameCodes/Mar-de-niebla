3e109a4a51dd99084e946759dfcae497fd88a7f3
irina-p-d <irinapetrovap@gmail.com>
2022-03-08 12:44:35 +0100

    card style done

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,12 +5,13 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <x-card></x-card>
+    <div class="">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <x-jet-welcome />
             </div>
         </div>
     </div>
-    <x-card></x-card>
+    
 </x-app-layout>
