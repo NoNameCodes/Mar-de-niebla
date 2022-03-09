7a8517126a679046f4c1521041695c538a8218dc
DanielgDiaz <danielgdiaz92@gmail.com>
2022-03-09 14:13:08 +0100

    view edit crud resource

---
+++ resources/views/resourcesCrud/edit.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Edit Resource Form</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+</head>
+<body>
+<div class="container mt-2">
+<div class="row">
+<div class="col-lg-12 margin-tb">
+<div class="pull-left">
+<h2>Edit Resource</h2>
+</div>
+<div class="pull-right">
+<a class="btn btn-primary" href="{{ route('resources.index') }}" enctype="multipart/form-data"> Back</a>
+</div>
+</div>
+</div>
+@if(session('status'))
+<div class="alert alert-success mb-1 mt-1">
+{{ session('status') }}
+</div>
+@endif
+<form action="{{ route('resources.update',$resource->id) }}" method="POST" enctype="multipart/form-data">
+@csrf
+@method('PUT')
+<div class="row">
+<div class="col-xs-12 col-sm-12 col-md-12">
+<div class="form-group">
+<strong>Resource Name:</strong>
+<input type="text" name="name" value="{{ $resource->name }}" class="form-control" placeholder="Resource name">
+@error('name')
+<div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+@enderror
+</div>
+</div>
+<div class="col-xs-12 col-sm-12 col-md-12">
+<div class="form-group">
+<strong>Resource Email:</strong>
+<input type="email" name="email" class="form-control" placeholder="Resource Email" value="{{ $resource->email }}">
+@error('email')
+<div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+@enderror
+</div>
+</div>
+<div class="col-xs-12 col-sm-12 col-md-12">
+<div class="form-group">
+<strong>Resource Address:</strong>
+<input type="text" name="address" value="{{ $resource->address }}" class="form-control" placeholder="Resource Address">
+@error('address')
+<div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+@enderror
+</div>
+</div>
+<button type="submit" class="btn btn-primary ml-3">Submit</button>
+</div>
+</form>
+</div>
+</body>
+</html>
