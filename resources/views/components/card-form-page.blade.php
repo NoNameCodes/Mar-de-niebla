7a32698f5edf0f37bf79e1c09cb98e82911a5b1e
Albazc <albazcano@gmail.com>
2022-03-11 13:06:03 +0100

    modified cardFormPage, cardMultiselect and mixing app.css

--- resources/views/components/card-form-page.blade.php
+++ resources/views/components/card-form-page.blade.php
@@ -1,20 +1,19 @@
 <div class="w-80 h-100 bg-white border-orange-400 border-2 rounded-md mx-10 mb-3">
   <div class=" bg-white rounded-md px-9 py-10 max-w-2xl">
       <div class="space-y-4 px-15">
-         <div>
-          <input type="text" placeholder="Nombre del recurso" class=" outline-none px-12 py-3 text-sm flex border-orange-400 border-1 rounded-md" />
-         </div>
-         
-          <div>
-           <input type="text" placeholder="Localización" class=" outline-none px-12 pt-3 text-sm border-orange-400 border-1 rounded-md" />
-         </div>
 
-        
+        <div class="mb-3 pt-0">
+          <input type="text" placeholder="Nombre del recurso" class="px-3 py-3 mt-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-base border border-orange-400 outline-none focus:outline-none focus:ring w-full"/>
+        </div>
+         
          <x-card-multiselect :users="$users" />
 
+         <div class="mb-3 pt-0">
+          <input type="text" placeholder="Localización" class="px-3 py-3 mt-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-base border border-orange-400 outline-none focus:outline-none focus:ring w-full"/>
+        </div>
 
          <div class="py-1 mt-3">
-           <textarea id="description" cols="31" rows="10" placeholder="Descripción/Incidencias" class=" outline-none px-2 text-sm border-orange-400 border-1 rounded-md"></textarea>
+           <textarea id="description" cols="26" rows="10" placeholder="Descripción/Incidencias" class=" outline-none px-2 text-m border-orange-400 border-1 rounded-md"></textarea>
          </div> 
          
          <div class="py-1">
