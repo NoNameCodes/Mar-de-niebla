739a1f56d0f7df2fb4b7dfba066cc14245bc282d
Albazc <albazcano@gmail.com>
2022-03-13 19:35:17 +0100

    modified Form route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserPendingController;
+use App\Http\Controllers\FormController;
 
 /*
 |--------------------------------------------------------------------------
