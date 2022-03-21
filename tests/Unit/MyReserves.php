c88cd0a891d9b5a2043177b4efa622995dcf9be9
Cortina17 <diego.cortinagarcia@gmail.com>
2022-03-21 12:48:28 +0100

    test user not logged

--- tests/Unit/MyReserves.php
+++ tests/Unit/MyReserves.php
@@ -2,17 +2,22 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class MyReserves extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic unit test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_without_auth_returns_login()
     {
-        $this->assertTrue(true);
+        $response = $this->get('/dashboard');
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
     }
 }
