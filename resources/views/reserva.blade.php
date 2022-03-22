ef9845a13713df18036dad396c90ca361549c831
SashaBidardel <sashabidardel@gamil.com>
2022-03-22 11:48:23 +0100

    adding calendar changes

--- resources/views/reserva.blade.php
+++ resources/views/reserva.blade.php
@@ -23,8 +23,14 @@
       
   
     </x-header >
+    <div class="flex justify-center text-red-700 ">
+      @if (Session::has('message'))
+         {{(session('message'))}}
+
+      @endif
+     </div>
     
-     <div class="flex flex-col justify-center items-center">
+    <div class="flex flex-col justify-center items-center">
       <x-resource-show :resource="$resource" /> 
      </div>
      
@@ -37,10 +43,10 @@
             
               <input  type="date"
                 class="form-control flex items-center text-gray-700   border-orange-500 rounded-xl  bg-[#FAF3F3] mb-6" 
-                  id="date" name="date"/> 
+                  id="date" name="date" min="<?php echo date("Y-m-d");?>" required /> 
             
-        <input type="text" placeholder="Introduce tu nombre" class="text-gray-700 italic border-orange-500 rounded-xl  bg-[#FAF3F3]  mb-6 " name="name" >
-        <input type="number"   placeholder="Introduce tu teléfono" class="text-gray-700 italic border-orange-500 rounded-xl  bg-[#FAF3F3] mb-6" name="phone"  >
+        <input type="text" placeholder="Introduce tu nombre" class="text-gray-700 italic border-orange-500 rounded-xl  bg-[#FAF3F3]  mb-6 " name="name"required>
+        <input type="number"   placeholder="Introduce tu teléfono" class="text-gray-700 italic border-orange-500 rounded-xl  bg-[#FAF3F3] mb-6" name="phone" required >
         <textarea class="text-gray-700 italic border-orange-500 rounded-xl bg-[#FAF3F3] py-12 mb-6" placeholder="Comentarios" name="coments"></textarea>
         <button class="bg-[#F8981D] hover:bg-[#ecc188] text-white font-bold py-2 px-8 shadow-xl rounded-full mb-2" type="submit">
           Reservar
