ef0fb519fadd2ef965b70dcde6d252d618e7b69a
DanielgDiaz <danielgdiaz92@gmail.com>
2022-03-22 14:07:15 +0100

    tested login

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Browser;
+use App\Models\User;
+use Laravel\Dusk\Chrome;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+    
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function test_user_login()
+    {
+
+     
+
+        $this->browse(function (Browser $browser)  {
+            $browser->visit('/')
+                    ->waitForText('Login')
+                    ->type('email', value: 'dani@dani.dani')
+                    ->type('password', value: '12345678')
+                    ->click('#btnEnter')
+                    ->assertPathIs('/home');
+        });
+    }
+}
