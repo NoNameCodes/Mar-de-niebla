855ea58ce37d8cb72946c35c75c115d3ff0b9233
Caio Da Silva <caiodasilvatx@gmail.com>
2022-03-24 14:52:33 +0100

    conflicts solved

--- resources/views/components/pending-aproval.blade.php
+++ resources/views/components/pending-aproval.blade.php
@@ -4,70 +4,6 @@
         <button type="button" data-dropdown-toggle="dropdown"><i class="fa-solid fa-user fa-2x "
                 style="color: #F8981D"></i> </button>
 
-<<<<<<< HEAD
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            @if (session('message'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('message') }}
-                </div>
-            @endif
-            <div class="flex flex-col">
-                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                        <div class="shadow overflow-scroll border-b border-gray-200 rounded-lg">
-                            <table class="min-w-full divide-y divide-gray-200">
-                                <thead class="bg-gray-50">
-                                    <tr>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Nombre</th>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Email</th>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Fecha de Creación</th>
-                                        <th scope="col" class="relative px-6 py-3">
-                                            <span class="sr-only"></span>
-                                        </th>
-                                    </tr>
-                                </thead>
-                                @forelse ($users as $user)
-                                    <tbody class="bg-white divide-y divide-gray-200">
-                                        <tr>
-                                            <td class="px-6 py-4 whitespace-nowrap">
-                                                <div class="flex items-center">
-                                                    <div class="flex-shrink-0 h-10 w-10">
-                                                        <img class="h-10 w-10 rounded-full" src="" alt="">
-                                                    </div>
-                                                    <div class="ml-4">
-                                                        <div class="text-sm font-medium text-gray-900">
-                                                            {{ $user->name }}</div>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td class="px-6 py-4 whitespace-nowrap">
-                                                <div class="text-sm text-gray-900">
-                                                    {{ $user->email }}</div>
-                                            </td>
-                                            <td class="px-6 py-4 whitespace-nowrap">
-                                                <span
-                                                    class="px-2 inline-flex text-xs leading-5 font-semibold">{{ $user->created_at }}</span>
-                                            </td>
-                                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                                <a id='btnAceptar' href="{{ route('admin.users.approve', $user->id) }}"
-                                                    class="text-indigo-600 hover:text-indigo-900">Aceptar</a>
-                                        </tr>
-                                    @empty
-                                        <tr>
-                                            <td>No users found.</td>
-                                        </tr>
-                                    </tbody>
-                                @endforelse
-                            </table>
-                        </div>
-=======
         <!-- Dropdown menu -->
         <div class="hidden bg-[#F8981D] text-base z-50  " id="dropdown">
 
@@ -120,7 +56,6 @@
                                     clip-rule="evenodd" />
                             </svg>
                         </a>
->>>>>>> restyle/caio-admin-dashboard
                     </div>
                 </div>
             </div>
