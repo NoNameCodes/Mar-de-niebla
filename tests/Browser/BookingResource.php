6267465056a2759d456280c25fc9dfcc449b0007
DanielgDiaz <danielgdiaz92@gmail.com>
2022-03-25 12:44:55 +0100

    test booking resource ✔

--- tests/Browser/BookingResource.php
+++ tests/Browser/BookingResource.php
@@ -13,7 +13,7 @@ class BookingResource extends DuskTestCase
      *
      * @return void
      */
-    public function testExample()
+    public function test_booking_resource()
     {
         $this->browse(function (Browser $browser)  {
             $browser->visit('/')
@@ -24,10 +24,12 @@ class BookingResource extends DuskTestCase
                     ->assertPathIs('/home')
                     ->click('#btnBooking')
                     ->assertSee('Seleccione el día')
-                    ->type('date', value:'12122022')
+                    ->type('date', value:'10122022')
                     ->type('text', value:'dani')
                     ->type('number', value:'678')
                     ->type('textarea', value:'678')
+                    ->click('#btnReserve')
+                    ->assertSee('Ha realizado correctamente la reserva');
         });
     }
 }
