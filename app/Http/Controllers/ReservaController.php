a118fd2d90f724c8c3c34f20f6ad814e803d21e7
Cortina17 <diego.cortinagarcia@gmail.com>
2022-03-25 11:33:51 +0100

    added back() function to reserve

--- app/Http/Controllers/ReservaController.php
+++ app/Http/Controllers/ReservaController.php
@@ -10,73 +10,49 @@ use Illuminate\Support\Facades\Session;
 
 class ReservaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $resources = Resource::orderBy('id', 'desc');
         return view('reserva', compact('resources'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create($id)
     {
-        $resource=Resource::find($id);
-        return view('reserva', ['resource'=>$resource]);
+        $resource = Resource::find($id);
+        return view('reserva', ['resource' => $resource]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request,$id)
+    public function store(Request $request, $id)
     {
-            $reservasTotales=Reserva::all();
-            $resource=Resource::find($id);
-            $user=Auth::user()->id;
-            $reserva=new Reserva();
-            $reserva->name=$request->input('name');
-            $reserva->date=$request->input('date');
-            $reserva->coments=$request->input('coments');
-            $reserva->phone=$request->input('phone');
-            $reserva->location_id=$resource->location_id;
-            $reserva->resource_id=$id;
-            $reserva->user_id=$user;
-            foreach ($reservasTotales as $variable) {
-                if (($variable->date==$reserva->date) & ($variable->resource_id==$reserva->resource_id)){
-                    $message="La reserva del recurso ".$resource->name." para el día: ".strval($reserva->date)." está ocupada.Por favor,eliga otra fecha";
-                    Session::flash('message',$message);
-                    return redirect()->route('home');
-                }
+        $reservasTotales = Reserva::all();
+        $resource = Resource::find($id);
+        $user = Auth::user()->id;
+        $reserva = new Reserva();
+        $reserva->name = $request->input('name');
+        $reserva->date = $request->input('date');
+        $reserva->coments = $request->input('coments');
+        $reserva->phone = $request->input('phone');
+        $reserva->location_id = $resource->location_id;
+        $reserva->resource_id = $id;
+        $reserva->user_id = $user;
+        foreach ($reservasTotales as $variable) {
+            if (($variable->date == $reserva->date) & ($variable->resource_id == $reserva->resource_id)) {
+                $message = "La reserva del recurso " . $resource->name . " para el día: " . strval($reserva->date) . " está ocupada.Por favor,eliga otra fecha";
+                Session::flash('message', $message);
+                return back();
             }
-            $reserva->save();
-            $message="Ha realizado correctamente la reserva del recurso ".$resource->name." para el día: ".strval($reserva->date);
-            Session::flash('message',$message);
-            return redirect()->route('home');
-           
-        
+        }
+        $reserva->save();
+        $message = "Ha realizado correctamente la reserva del recurso " . $resource->name . " para el día: " . strval($reserva->date);
+        Session::flash('message', $message);
+        return redirect()->route('home');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        
-        $resource=Resource::find($id);
-        return view('reserva',compact('resource'));
+
+        $resource = Resource::find($id);
+        return view('reserva', compact('resource'));
     }
 
     /**
