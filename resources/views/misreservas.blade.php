649a5ccffbf799bd6963d8ec76bda064a883f583
Caio Da Silva <caiodasilvatx@gmail.com>
2022-03-25 12:18:23 +0100

    added an empty condition to the forelse

--- resources/views/misreservas.blade.php
+++ resources/views/misreservas.blade.php
@@ -18,27 +18,35 @@
         }
 
     </style>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 </head>
 
-<x-header />
-  
-
-<x-button-add />
-
-<main class="flex justify-center mb-3">
-    <div class="ms:w-4/5 md:w-4/5 lg:w-2/5">
-        <div class="border-2 border-[#F8981D] rounded-xl m-1.5 py-1">
-            @foreach ($resources as $resource)
-                <x-card :resource="$resource" />
-            @endforeach
-            <div>{{ $resources->links() }}</div>
+<body>
+    <x-header />
+    <main class="flex flex-col items-center">
+        <x-button-add />
+        <div class="ms:w-4/5 md:w-4/5 lg:w-2/5">
+            <div class="border-2 border-[#F8981D] rounded-xl m-1.5 py-1">
+                @forelse ($resources as $resource)
+                    <x-card :resource="$resource" />
+                @empty
+                    <div class="flex flex-col items-center m-5">
+                        <p class="flex justify-center rounded-md border-2 border-gray-400 bg-gray-200 p-6">No tienes
+                            nada
+                            reservado
+                            aún!
+                        </p>
+                        <button id="btnAdd"
+                            class="mt-5 px-6 py-1 mx-auto block rounded-md text-md font-semibold text-white bg-[#F8981D] drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]"><a
+                                href="{{ url('/home') }}">Recursos disponibles</a></button>
+                    </div>
+                @endforelse
+                <div>{{ $resources->links() }}</div>
+            </div>
         </div>
-    </div>
-</main>
-
-<x-footer />
-
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-    integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    </main>
+    <x-footer />
+</body>
 
 </html>
