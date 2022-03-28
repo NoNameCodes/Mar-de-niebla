922ca5ec85854442914f8fb151cac19a202513d5
irina-p-d <irinapetrovap@gmail.com>
2022-03-28 09:22:24 +0200

    working to connect BD with card

--- app/Http/Controllers/MisreservasController.php
+++ app/Http/Controllers/MisreservasController.php
@@ -3,23 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\Reserva;
-
+use App\Models\Resource;
 use Illuminate\Support\Facades\Auth;
+use phpDocumentor\Reflection\Types\Resource_;
 
 class MisreservasController extends Controller
 {
     public function index()
     {
         $user = Auth::user();
-        $resources = Reserva::where('user_id', $user->id)->get();
-        $resources = $resources->resource;
+        $reservas = Reserva::where('user_id', $user->id)->get();
+        $resources = Resource::find($user->resource_id);
+        dd($resources);
+        
         return view('misreservas', compact('resources'));
     }
     
-
+    
     
 
-    
-       
 }
 
