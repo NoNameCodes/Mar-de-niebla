46a6424fb65fdf17d70c11ffd10f39b4a70b832c
Cortina17 <diego.cortinagarcia@gmail.com>
2022-03-28 15:11:57 +0200

    added middleware
    middleware checks if user or admin and only redirect to allowed views

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\ReservaController;
 use App\Http\Controllers\MisreservasController;
 
-Route::middleware(['auth:sanctum', 'verified', 'approved'])->group(function () {
+Route::middleware(['auth:sanctum', 'verified', 'approved', 'user'])->group(function () {
     Route::get('/', [HomeController::class, 'index'])->name('home');
     Route::get('/home', [HomeController::class, 'index'])->name('home');
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');;
@@ -26,10 +26,11 @@ Route::middleware(['auth:sanctum', 'verified', 'approved'])->group(function () {
     Route::get('/approval', [HomeController::class, 'approval'])->name('approval');
     Route::get('/misreservas', [MisreservasController::class, 'index'])->name('misreservas');
 
-    Route::middleware(['admin'])->group(function () {
-        Route::get('aprove/users', [UserPendingController::class, 'index'])->name('admin.users.pending.index');
-        Route::get('aprove/users/{user_id}/', [UserPendingController::class, 'approve'])->name('admin.users.approve');
-        Route::get('users', [UserController::class, 'index'])->name('admin.users.index');
-        Route::get('users/{user_id}/delete', [UserController::class, 'delete'])->name('admin.users.delete');
-    });
+});
+
+Route::middleware(['auth:sanctum', 'verified', 'approved','admin'])->group(function () {
+    Route::get('users', [UserController::class, 'index'])->name('admin.users.index');
+    Route::get('users/{user_id}/delete', [UserController::class, 'delete'])->name('admin.users.delete');
+    Route::get('aprove/users', [UserPendingController::class, 'index'])->name('admin.users.pending.index');
+    Route::get('aprove/users/{user_id}/', [UserPendingController::class, 'approve'])->name('admin.users.approve');
 });
