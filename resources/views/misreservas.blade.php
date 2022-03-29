4a4e5f01b86b57bbfe15821e7ca33e8049eead0d
DanielgDiaz <danielgdiaz92@gmail.com>
2022-03-29 12:22:55 +0200

    hover & shadow buttons :art:

--- resources/views/misreservas.blade.php
+++ resources/views/misreservas.blade.php
@@ -43,7 +43,7 @@
                             aún!
                         </p>
                         <button id="btnAdd"
-                            class="mt-5 px-6 py-1 mx-auto block rounded-md text-md font-semibold text-white bg-[#F8981D] drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]"><a
+                            class="mt-5 px-6 py-1 mx-auto block rounded-md text-md font-semibold text-white bg-[#F8981D] hover:bg-[#ffba25] duration-300 drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]"><a
                                 href="{{ url('/home') }}">Recursos disponibles</a></button>
                     </div>
                 @endforelse
