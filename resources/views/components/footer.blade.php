1ad5cbeb49b724d5b9bd199ff6519485f8481ff6
DanielgDiaz <danielgdiaz92@gmail.com>
2022-03-29 11:11:42 +0200

    logo footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,6 @@
 <div>
-    <footer class="text-center p-6 bg-black fixed inset-x-0 bottom-0 w-full">
-        <p class="text-sm text-white"> Fundación por la Acción Social Mar de Niebla - 2022</p>
+    <footer class="flex justify-center p-6 bg-black fixed inset-x-0 bottom-0 w-full">
+        <img draggable="false" src="{{ asset('storage/logo.png') }}" class="h-9" alt="logo">
+        {{-- <p class="text-sm text-white"> Fundación por la Acción Social Mar de Niebla - 2022</p> --}}
     </footer>
 </div>
