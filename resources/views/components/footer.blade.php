e14627e7b5dbe26b61d173728793b73f03b827c7
DanielgDiaz <danielgdiaz92@gmail.com>
2022-03-29 11:19:52 +0200

    href logo footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,7 @@
 <div>
     <footer class="flex justify-center p-6 bg-black fixed inset-x-0 bottom-0 w-full">
-        <img draggable="false" src="{{ asset('storage/logo.png') }}" class="h-9" alt="logo">
-        {{-- <p class="text-sm text-white"> Fundación por la Acción Social Mar de Niebla - 2022</p> --}}
+        <a href="{{ route('home') }}">
+            <img draggable="false" src="{{ asset('storage/logo.png') }}" class="h-9" alt="logo">
+        </a>
     </footer>
 </div>
