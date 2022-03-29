40a3472b06d976936138b77373c1f299898e65d3
Caio Da Silva <caiodasilvatx@gmail.com>
2022-03-29 09:44:07 +0200

    changed the footer text

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <footer class="text-center p-6 bg-black fixed inset-x-0 bottom-0 w-full">
-        <p class="text-sm text-white"> Fundación por la Acción Social Mar de Niebla - 2022</p>
+    <footer class="text-center p-6 bg-black inset-x-0 bottom-0 w-full">
+        <p class="text-sm text-white"> Barrio Oeste - 2022</p>
     </footer>
 </div>
