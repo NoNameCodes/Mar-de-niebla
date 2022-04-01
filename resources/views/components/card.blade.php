353540cadc7cb6145fb8d2d3ac696a9f57b6112e
Caio Da Silva <caiodasilvatx@gmail.com>
2022-04-01 15:09:44 +0200

    changed the resource src

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -16,7 +16,7 @@
         <div class="ms:w-90 md:w-90 border-b-2 border-[#faba67] flex">
             <div class="w-3/12 p-4">
                 <a href="{{ route('resource', $resource) }}">
-                    <img src="img/{{ $resource->img }}" />
+                    <img src="storage/img/{{ $resource->img }}" />
                 </a>
             </div>
             <div class="flex justify-between w-3/4">
@@ -50,9 +50,16 @@
                             </form>
                         </div>
                     @elseif (Route::currentRouteName() == 'misreservas')
-                    <button class="btn inline-block select-none no-underline align-middle cursor-pointer whitespace-nowrap px-4 py-1.5 rounded text-base font-medium leading-6 tracking-tight text-white text-center border-0 bg-[#F8981D] hover:bg-[#ffba25]" type="button" onclick="location.href = '{{ route('removereserve', $reserva->id) }}'">Anular Reserva</button>
+                        <button
+                            class="btn inline-block select-none no-underline align-middle cursor-pointer whitespace-nowrap px-4 py-1.5 rounded text-base font-medium leading-6 tracking-tight text-white text-center border-0 bg-[#F8981D] hover:bg-[#ffba25]"
+                            type="button"
+                            onclick="location.href = '{{ route('removereserve', $reserva->id) }}'">Anular
+                            Reserva</button>
                     @else
-                    <button class="btn inline-block select-none no-underline align-middle cursor-pointer whitespace-nowrap px-4 py-1.5 rounded text-base font-medium leading-6 tracking-tight text-white text-center border-0 bg-[#92C954] hover:bg-[#85f307] duration-300" type="button" onclick="location.href = '{{ route('reserva.create', $resource->id) }}'">Reservar</button>
+                        <button
+                            class="btn inline-block select-none no-underline align-middle cursor-pointer whitespace-nowrap px-4 py-1.5 rounded text-base font-medium leading-6 tracking-tight text-white text-center border-0 bg-[#92C954] hover:bg-[#85f307] duration-300"
+                            type="button"
+                            onclick="location.href = '{{ route('reserva.create', $resource->id) }}'">Reservar</button>
                     @endif
                 </div>
             </div>
