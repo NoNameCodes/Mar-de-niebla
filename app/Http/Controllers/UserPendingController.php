2f93fe856788667ba69279035c459543a62acb47
Caio Da Silva <caiodasilvatx@gmail.com>
2022-04-05 11:37:46 +0200

    new reject function

--- app/Http/Controllers/UserPendingController.php
+++ app/Http/Controllers/UserPendingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Notifications\RegisterRejected;
 
 class UserPendingController extends Controller
 {
@@ -18,5 +19,13 @@ class UserPendingController extends Controller
         $user = User::findOrFail($user_id);
         $user->update(['aproved_at' => now()]);
         return redirect()->route('admin.users.pending.index')->withMessage('¡Organización aceptada correctamente!');
+    }
+
+    public function reject($user_id)
+    {
+        $user = User::findOrFail($user_id);
+        $user->notify(new RegisterRejected);
+        $user->delete();
+        return redirect()->route('admin.users.pending.index')->withMessage('¡Organización rechazada correctamente!');
     }
 }
