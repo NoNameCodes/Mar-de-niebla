c3f7ecaddbad3619b1486aa28b97390daf88a2cb
Albazc <albazcano@gmail.com>
2022-04-06 01:40:11 +0200

    finishing resizing inputs

--- resources/views/components/location.blade.php
+++ resources/views/components/location.blade.php
@@ -1,6 +1,6 @@
   <div class="relative flex w-full" >
   
-    <select class="border border-orange-400 rounded-md text-gray-600 h-11 px-8 bg-white hover:border-gray-400 focus:outline-none appearance-none" name=location>
+    <select class="border border-orange-400 rounded-md text-gray-600 h-10 px-8 bg-white hover:border-gray-400 focus:outline-none appearance-none" name=location>
       <option disabled selected>--Localización--</option>
       @foreach ($location as $location)
       <option value="{{$location->id}}">{{$location->address}}</option>
