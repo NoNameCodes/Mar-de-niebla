3ffeec7791742c3907ff07c28854159f329d44f0
SashaBidardel <sashabidardel@gamil.com>
2022-04-05 10:24:46 +0200

    adding location controller

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -3,12 +3,43 @@
 namespace App\Http\Controllers;
 
 use App\Models\Location;
+use Illuminate\Http\Request;
 
 class LocationController extends Controller
 {
     public function index(){
         $location = Location::get();
         return view ('card-form-page', compact('location'));
+    }
+    public function vista(){
+        $location = Location::get();
+        return view('location_view',compact('location'));
+    }
+    public function create(){
+        return view('location_create');
+    }
+    public function store(Request $request){
+        $location= new Location(); 
+        $location->address = $request->input('address');
+        $location->resource_id=2;
+        $location->save();
+        return  redirect ('/locations');
+    }
+    public function edit($id){
+        $location=Location::find($id);
+        return view('locationedit', compact('location'));
+
+    }
+    public function update(Request $request,$id){
+        $location = Location::find($id);
+        $location->address = $request->input('address');
+        $location->save();
+        return  redirect ('/locations');
+    }
+    public function destroy($id){
+        $location=Location::find($id);
+        $location->delete();
+        return redirect('/locations');
     }
 }
 
