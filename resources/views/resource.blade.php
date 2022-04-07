83869053bed690768b90675c0d36c9189704322e
Cortina17 <diego.cortinagarcia@gmail.com>
2022-04-07 14:06:37 +0200

    route form

--- resources/views/resource.blade.php
+++ resources/views/resource.blade.php
@@ -14,7 +14,8 @@
             <div class="flex flex-col justify-center p-2 mt-10">
                 <button
                     class="btn  inline-block select-none no-underline align-middle  cursor-pointer whitespace-nowrap px-4 py-1.5 rounded text-base mt-6 font-medium leading-6 tracking-tight text-white text-center border-0 bg-[#92C954] hover:bg-[#85f307] duration-300"
-                    type="button">Reservar</button>
+                    type="button"
+                    onclick="location.href = '{{ route('reserva.create', $resource->id) }}'">Reservar</button>
             </div>
         </div>
     </div>
