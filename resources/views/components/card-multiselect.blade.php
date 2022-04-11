61e60147081dc5f835667d96a0663bc244448022
Albazc <albazcano@gmail.com>
2022-04-12 00:12:20 +0200

    modified cardForm and cardMultiselect

--- resources/views/components/card-multiselect.blade.php
+++ resources/views/components/card-multiselect.blade.php
@@ -1,11 +1,43 @@
-<h2>Compartir con</h2>
+  <!-- component -->
+<button class="bg-[#F8981D] text-white rounded-md px-16 py-2 hover:bg[#F8981D] transition" onclick="openModal('modal')">Compartir con</button>
 
-<form action="/action_page.php">
-    <div class="dropdown">
-         @foreach ($users as $user)
-            <input type="checkbox" name="{{$user->id}}" value="{{$user->id}}">
-            <label for="{{$user->id}}"> {{$user->name}}</label><br>
-            @endforeach
+<div id="modal" class="fixed hidden z-50 inset-0 bg-gray-900 bg-opacity-60 overflow-y-auto h-full w-full px-4">
+    <div class="relative top-40 mx-auto shadow-lg rounded-md bg-white max-w-md">
+
+        <!-- Modal header -->
+        <div class="flex justify-between items-center bg-[#F8981D] text-white text-xl rounded-t-md px-4 py-2">
+            <h3>Compartir con</h3>
+            <button onclick="closeModal()">x</button>
+        </div>
+
+        <!-- Modal body -->
+        <div class="max-h-48 overflow-y-scroll p-4">
+            <form action="/action_page.php">
+                <div class="dropdown">
+                     @foreach ($users as $user)
+                        <input type="checkbox" name="{{$user->id}}" value="{{$user->id}}">
+                        <label for="{{$user->id}}"> {{$user->name}}</label><br>
+                        @endforeach
+                </div>
+                    
+              </form>
+        </div>
+
+        <!-- Modal footer -->
+        <div class="px-4 py-2 border-t border-t-gray-500 flex justify-end items-center space-x-4">
+            <button class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition" onclick="closeModal()">Close</button>
+        </div>
     </div>
-        
-  </form>
+</div>
+
+<script type="text/javascript">
+function openModal(modalId) {
+    modal = document.getElementById(modalId)
+    modal.classList.remove('hidden')
+}
+
+function closeModal() {
+    modal = document.getElementById('modal')
+    modal.classList.add('hidden')
+}
+</script>
