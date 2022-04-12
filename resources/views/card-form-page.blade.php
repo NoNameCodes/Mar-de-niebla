7037d0f7813a87bb63c0b246b2a1a7b0ef7dfb52
Albazc <albazcano@gmail.com>
2022-04-12 11:13:34 +0200

    modified cardFormPage

--- resources/views/card-form-page.blade.php
+++ resources/views/card-form-page.blade.php
@@ -8,7 +8,7 @@
                 <div class=" bg-white rounded-md px-9 py-10 max-w-2xl">
                     <div class="space-y-4 px-15">
 
-                        <div class="mb-3 pt-0">
+                        <div class="mb-1 pt-0">
                             <input type="text" name="name" placeholder="Nombre del recurso"
                                 class="px-3 py-2 mt-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-m border-orange-400 border-1 rounded-md outline-none focus:outline-none focus:ring w-full" required/>
                         </div>
@@ -18,7 +18,7 @@
                         <x-location :location="$location" />
 
                         <div class="py-1 mb-0">
-                            <textarea id="description" cols="24" rows="5" name="description" placeholder="Descripción/Incidencias"
+                            <textarea id="description" cols="25" rows="5" name="description" placeholder="Descripción/Incidencias"
                                 class=" outline-none px-2 text-m border-orange-400 border-1 rounded-md" required></textarea>
                         </div>
 
