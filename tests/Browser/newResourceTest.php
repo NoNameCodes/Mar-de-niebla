933c11252422e714eca21cbe51062196226e8332
Albazc <albazcano@gmail.com>
2022-04-13 15:58:04 +0200

    modified tests

--- tests/Browser/newResourceTest.php
+++ tests/Browser/newResourceTest.php
@@ -8,54 +8,7 @@ use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
 class newResourceTest extends DuskTestCase
-{
-
-    /**
-     * A Dusk test example.
-     *
-     * @return void
-     */
-    
-     public function testFormName()
-    {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                ->type('email', value: 'm@m')
-                ->type('password', value: 'password')
-                ->click('#btnEnter')
-                ->assertPathIs('/')
-                ->click('#btnAdd')
-                // ->assertSee('Nombre del Recurso')
-                ->type('text', 'Furgoneta Grande');
-        });
-    }
-
-    public function testDropdown()
-    {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                ->type('email', value: 'm@m')
-                ->type('password', value: 'password')
-                ->click('#btnEnter')
-                ->assertPathIs('/')
-                ->click('#btnAdd')
-                ->check('Mar de Niebla');
-        });
-    }
-
-    public function testImageupload()
-    {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-            ->type('email', value: 'm@m')
-            ->type('password', value: 'password')
-            ->click('#btnEnter')
-            ->assertPathIs('/')
-            ->click('#btnAdd')
-            ->attach('photo', __DIR__.'1649759240_furgoneta-grande-del-transporte-aislada.jpg');
-        });
-    }
-
+{  
     public function testFormSave()
     {
         $this->browse(function (Browser $browser) {
