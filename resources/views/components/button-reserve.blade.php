e91d6a889a34a18df6a84f850271916cbee70521
Cortina17 <diego.cortinagarcia@gmail.com>
2022-04-13 13:07:28 +0200

    more buttons and styles

---
+++ resources/views/components/button-reserve.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <button id="btnReserve"
+        class="bg-[#F8981D] hover:bg-[#ecc188] text-white font-bold py-2 px-8 shadow-xl rounded-full mb-2" type="submit">
+        Reservar
+    </button>
+</div>
