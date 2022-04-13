25d4de7f02a7ce3d8c0547114e38d8c30ffeb170
Albazc <albazcano@gmail.com>
2022-04-13 11:53:25 +0200

    modified closeButton

--- resources/views/card-form-page.blade.php
+++ resources/views/card-form-page.blade.php
@@ -10,7 +10,8 @@
 
                         <div class="mb-1 pt-0">
                             <input type="text" name="name" placeholder="Nombre del recurso"
-                                class="px-3 py-2 mt-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-m border-orange-400 border-1 rounded-md outline-none focus:outline-none focus:ring w-full" required/>
+                                class="px-3 py-2 mt-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-m border-orange-400 border-1 rounded-md outline-none focus:outline-none focus:ring w-full"
+                                required />
                         </div>
 
                         <x-card-multiselect :users="$users" />
@@ -19,19 +20,18 @@
 
                         <div class="py-1 mb-0">
                             <textarea id="description" cols="24" rows="5" name="description" placeholder="Descripción/Incidencias"
-                                class=" outline-none px-2 text-m border-orange-400 border-1 rounded-md" required></textarea>
+                                class=" outline-none px-2 text-m border-orange-400 border-1 rounded-md"
+                                required></textarea>
                         </div>
 
                         <div class="py-1">
                             <label class="block">
                                 <input name="img" type="file"
-                                    class="block w-full text-sm text-slate-500 file:mr-4 file:py-1 file:px-6 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-orange-500 file:text-white hover:file:bg-violet-100" required />
+                                    class="block w-full text-sm text-slate-500 file:mr-4 file:py-1 file:px-6 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-orange-500 file:text-white hover:file:bg-violet-100"
+                                    required />
                             </label>
                         </div>
-
-
                         <div class="grid grid-cols-2 gap-1 place-items-center h-26 mt-3">
-                            <x-button-close/>
                             <button type="submit"
                                 class="px-4 py-1 mx-auto block rounded-md text-md font-semibold text-white bg-[#92C954]">Guardar</button>
                         </div>
@@ -39,6 +39,7 @@
                 </div>
             </div>
         </form>
+        <x-button-close />
     </div>
 </x-guest-layout>
 
--- resources/views/resources/edit.blade.php
+++ resources/views/resources/edit.blade.php
@@ -24,7 +24,7 @@
                         class="form-control px-3 py-2 my-3 placeholder-blueGray-300 text-blueGray-400 relative bg-white rounded text-m border-orange-400 border-1 rounded-md outline-none focus:outline-none focus:ring w-full">
                 </div>
             </div>
-            
+
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Descripción:</strong>
@@ -33,7 +33,7 @@
                         class="form-control px-3 py-2 my-3 placeholder-blueGray-300 text-blueGray-400 relative bg-white rounded text-m border-orange-400 border-1 rounded-md outline-none focus:outline-none focus:ring w-full">
                 </div>
             </div>
-            <x-location-edit :location="$location" :resource="$resource"/>
+            <x-location-edit :location="$location" :resource="$resource" />
             <x-prueba :users="$users" />
             {{-- <x-card-multiselect :users="$users" /> --}}
             <div class="col-xs-12 col-sm-12 col-md-12">
@@ -46,26 +46,21 @@
                 </div>
             </div>
         </div>
-       
+
         <div class="col-xs-12 col-sm-12 col-md-12 py-1 m-2 flex flex-row justify-center">
             <label class="block">
                 <input name="img" type="file"
-                    class="block w-full text-sm text-slate-500 file:mr-4 file:py-1 file:px-6 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-orange-500 file:text-white hover:file:bg-violet-100 " required />
+                    class="block w-full text-sm text-slate-500 file:mr-4 file:py-1 file:px-6 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-orange-500 file:text-white hover:file:bg-violet-100 "
+                    required />
             </label>
         </div>
-        
-
-       
         <div class="col-xs-12 col-sm-12 col-md-12 flex flex-row justify-center mb-6">
             <div class="flex flex-row justify-center my-6">
-                <button class="px-6 py-1 mx-3 block rounded-md text-md font-semibold text-white bg-[#A9171C]">
-                    <a class="appearence-none" href="{{ route('dashboard') }}" enctype="multipart/form-data">
-                        Cancelar</a>
-                </button>
                 <button type="submit"
                     class="btn px-4 py-1 mx-3 block rounded-md text-md font-semibold text-white bg-[#92C954]">Guardar</button>
             </div>
         </div>
     </form>
+    <x-button-close />
 </div>
 <x-footer />
