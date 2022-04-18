d6b9c884acd0bf4922678d95836879988a7b2a84
Cortina17 <diego.cortinagarcia@gmail.com>
2022-04-18 09:09:41 +0200

    error in one word

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'failed' => 'Email o contraseña incorrectos. Por favor, inténtelo de neuvo.',
+    'failed' => 'Email o contraseña incorrectos. Por favor, inténtelo de nuevo.',
     'password' => 'La contraseña es incorrecta.',
     'throttle' => 'Demasiados intentos incorrectos. Por favor, pruebe de neuvo más tarde.',
 ];
